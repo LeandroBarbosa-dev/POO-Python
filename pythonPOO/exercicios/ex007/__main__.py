e39b1e34b056a2736cf72ede0671af948915e3a5
Leandro Barbosa <contato.barbosaads@gmail.com>
2026-08-10 06:04:04 -0300

    Aprendendo sobre Abstração em Python incluido 5 exercicios

---
+++ pythonPOO/exercicios/ex007/__main__.py
@@ -0,0 +1,21 @@
+from classes import Aluno, Professor, Funcionario
+
+def main():
+    a1 = Aluno("José", 17, "Informática", "T01")
+    a1.fazer_aniversario()
+    a1.fazer_matricula()
+
+    p1 = Professor("Samuel", 37, "Biologia", "Mestrado")
+    p1.fazer_aniversario()
+    p1.dar_aula()
+
+    f1 = Funcionario("Claudia", 27, "Secretária", "Secretaria")
+    f1.fazer_aniversario()
+    f1.bater_ponto()
+
+    a1.estudar()
+    p1.estudar()
+    f1.estudar()
+
+if __name__ == "__main__":
+    main()
